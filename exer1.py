5a2d947f8adccd1984bee9064247c88e083dcc08
louispawaon <lpawaon@gmail.com>
2023-04-24 09:31:42 +0800

    exer1 done

---
+++ exer1.py
@@ -0,0 +1,29 @@
+'''
+Exer1
+
+Instruction
+
+Write a program that accepts an input value (range: 60-100)
+an prints a message based on the following range of values:
+
+RANGE                   MESSAGE
+60-74                   Derp!
+75-84                   Good
+85-94                   Very Good
+95-100                  Excellent!
+any other value         Invalid Value
+
+'''
+
+value = int(input("Enter a value: "))
+
+if value >= 60 and value <= 74:
+    print("Derp!")
+elif value >= 75 and value <= 84:
+    print("Good")
+elif value >= 85 and value <= 94:
+    print("Very Good")
+elif value >= 95 and value <= 100:
+    print("Excellent!")
+else:
+    print("Invalid Value")
