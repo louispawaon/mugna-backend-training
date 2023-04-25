f38c6cd58491743f6014b726fe66a3c2cb2b3eb7
louispawaon <lpawaon@gmail.com>
2023-04-25 13:33:23 +0800

    add exercise 6.2

---
+++ exer6-2.py
@@ -0,0 +1,26 @@
+'''
+Exer 6-2
+
+Create a recursive function that would sort out a list of integers of undefined length.
+
+'''
+
+def sorter(given):
+    if len(given)<=1:
+        return given
+    
+     #quicksort algorithm
+
+    shift=given[0]
+    left=[]                
+    right=[]
+
+    for i in range(1, len(given)):
+        if given[i]<shift:
+            left.append(given[i])
+        else:
+            right.append(given[i])
+    return sorter(left) + [shift] + sorter(right)
+
+num_list=[1,20,56,78,33,2,90]
+print(sorter(num_list))
