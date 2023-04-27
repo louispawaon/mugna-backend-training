d0be81809fe1dcc706ffcaa0e0a3aa14146188ba
louispawaon <lpawaon@gmail.com>
2023-04-27 10:49:04 +0800

    add exercise 12

---
+++ exer12.py
@@ -0,0 +1,56 @@
+"""
+Exer 12
+
+Create an iterator named PrimeNumberDigit that would return the 
+prime number (starting with lowest possible value) that contains
+a given integer in its digits
+
+div  = PrimeNumberDigit(13)
+next(div)
+
+"""
+
+class PrimeNumberDigit:
+    def __init__(self, divisible):
+        self.divisible = divisible # 13
+        self.current = divisible 
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        while True:
+            if str(self.divisible) in str(self.current) and self.is_prime(self.current): # if 13 is in currentNum and currentNum is prime
+                prime_num = self.current
+                self.current += 1
+                return prime_num
+            else:
+                self.current += 1
+
+    def is_prime(self,num):
+        if num < 2:
+            return False
+        for x in range(2, num):
+            if num % x == 0:
+                return False
+        return True
+
+div = PrimeNumberDigit(13)
+print(next(div))
+print(next(div))
+print(next(div))
+print(next(div))
+print(next(div))
+print(next(div))
+    
+
+
+
+
+
+
+
+
+
+
+
